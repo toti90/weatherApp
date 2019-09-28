ab25ae2c936c80da5e1f170d5a6ac9de8c19695c
tothtomi <tothtomi7@gmail.com>
2019-09-28 18:24:29 +0200

    initialize folder structure

--- weatherApp/src/app/app-routing.module.ts
+++ weatherApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Pages/HomePage/home.component';
+import { LoginComponent } from './Pages/LoginPage/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- weatherApp/src/app/app.module.ts
+++ weatherApp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/HomePage/home.component';
+import { LoginComponent } from './Pages/LoginPage/login.component';
+import { CoreModule } from './Core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
